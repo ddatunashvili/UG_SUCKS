97ac8ff8ff13b8849afd2b9d4ccecd30501f1d08
ddatunashvili <davitidatunashvili98@gmail.com>
2022-11-30 15:49:10 +0400

    bug fix

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ function show() {
 
         var url = window.location.href;
         if (url == "https://my.ug.edu.ge/") {
-            document.querySelector("main").innerHTML = `<iframe style="width:100%;height:800px" src="https://www.youtube.com/embed/dQw4w9WgXcQ?rel=0&amp;controls=0&autoplay=1&mute=1&showinfo=0&modestbranding=0" title="Rick Astley - Never Gonna Give You Up (Official Music Video)" frameborder="0" allow="accelerometer; autoplay=True; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> `;
+            document.querySelector("main").innerHTML = `
+                <iframe style="width:100%;height:800px;" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&showinfo=0&controls=0" allowfullscreen  frameborder="0"  sandbox="allow-scripts allow-presentation allow-same-origin"
+            allow="autoplay; fullscreen; picture-in-picture; xr-spatial-tracking; clipboard-write"></iframe> `;
+
+        }else if(url == "https://my.ug.edu.ge/login"){
+           document.querySelector(".logo img").style.width = "200px"
         }
         try{
             document.querySelectorAll(".d-none").forEach((el)=>{      
@@ -16,15 +21,36 @@ function show() {
         }) 
         }catch{console.log("none");}
         
-        document.querySelector("nav").innerHTML = `<nav data-v-31e7222c="" class="navbar navbar-expand-md navbar__lg  fixed-top "><div data-v-31e7222c="" class="sm-w-100 d-flex order-1 order-md-2"><button data-v-31e7222c="" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler collapsed"><span data-v-31e7222c="" class="navbar-toggler-icon"></span></button> </div> <div data-v-31e7222c="" id="navbarSupportedContent" class="collapse navbar-collapse order-2 order-md-1 navbarSupportedContent"><ul data-v-31e7222c="" class="navbar-nav"><li data-v-31e7222c="" class="nav-item ">
-<a data-v-31e7222c="" href="/schedule" class="nav-link text-nowrap" id="navbarDropdown_lg-0" role="button" aria-haspopup="true" aria-expanded="false"><img data-v-31e7222c="" src="https://drive.google.com/uc?export=view&amp;id=1Csxh6szRXpu0ye3fDo0_cWQTyp1xYVeu" alt="" class="img-fluid pre-icon">ცხრილი</a> </li><li data-v-31e7222c="" class="nav-item ">
-<a data-v-31e7222c="" href="/online-books" class="nav-link text-nowrap" id="navbarDropdown_lg-0" role="button" aria-haspopup="true" aria-expanded="false"><img data-v-31e7222c="" src="https://drive.google.com/uc?export=view&amp;id=1Csxh6szRXpu0ye3fDo0_cWQTyp1xYVeu" alt="" class="img-fluid pre-icon">წიგნები</a> </li><li data-v-31e7222c="" class="nav-item">
+        document.querySelector("nav").innerHTML = `<nav data-v-31e7222c="" class="navbar navbar-expand-md navbar__lg  fixed-top "><div data-v-31e7222c="" class="sm-w-100 d-flex order-1 order-md-2"><button data-v-31e7222c="" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler collapsed"><span data-v-31e7222c="" class="navbar-toggler-icon"></span></button> </div> <div data-v-31e7222c="" id="navbarSupportedContent" class="collapse navbar-collapse order-2 order-md-1 navbarSupportedContent">
+<ul data-v-31e7222c="" class="navbar-nav">
 
-<a data-v-31e7222c="" href="/exam-schedule" class="nav-link text-nowrap"><img data-v-31e7222c="" src="https://drive.google.com/uc?export=view&amp;id=1Csxh6szRXpu0ye3fDo0_cWQTyp1xYVeu" alt="" class="img-fluid pre-icon">გამოცდები</a> <!----></li><li data-v-31e7222c="" class="nav-item">
+    <li data-v-31e7222c="" class="nav-item ">
+        <a data-v-31e7222c="" href="/schedule" class="nav-link text-nowrap" id="navbarDropdown_lg-0" role="button" aria-haspopup="true" aria-expanded="false"><img data-v-31e7222c="" src="https://drive.google.com/uc?export=view&amp;id=1Csxh6szRXpu0ye3fDo0_cWQTyp1xYVeu" alt="" class="img-fluid pre-icon">ცხრილი</a> 
+    </li>
+    <li data-v-31e7222c="" class="nav-item ">
+        <a data-v-31e7222c="" href="/online-books" class="nav-link text-nowrap" id="navbarDropdown_lg-0" role="button" aria-haspopup="true" aria-expanded="false"><img data-v-31e7222c="" src="https://drive.google.com/uc?export=view&amp;id=1Csxh6szRXpu0ye3fDo0_cWQTyp1xYVeu" alt="" class="img-fluid pre-icon">წიგნები</a> 
+    </li>
 
-<a data-v-31e7222c="" href="/evaluations" class="nav-link text-nowrap"><img data-v-31e7222c="" src="https://drive.google.com/uc?export=view&amp;id=1Csxh6szRXpu0ye3fDo0_cWQTyp1xYVeu" alt="" class="img-fluid pre-icon">შეფასებები</a> <!----></li><li data-v-31e7222c="" class="nav-item">
+    <li data-v-31e7222c="" class="nav-item">
+        <a data-v-31e7222c="" href="/exam-schedule" class="nav-link text-nowrap"><img data-v-31e7222c="" src="https://drive.google.com/uc?export=view&amp;id=1Csxh6szRXpu0ye3fDo0_cWQTyp1xYVeu" alt="" class="img-fluid pre-icon">გამოცდები</a> <!---->
+    </li>
 
-<a data-v-31e7222c="" href="/my-applications/" class="nav-link text-nowrap"><img data-v-31e7222c="" src="https://drive.google.com/uc?export=view&amp;id=1Csxh6szRXpu0ye3fDo0_cWQTyp1xYVeu" alt="" class="img-fluid pre-icon">განცხადებები</a> <!----></li></ul></div></nav>`;
+    <li data-v-31e7222c="" class="nav-item">
+        <a data-v-31e7222c="" href="/evaluations" class="nav-link text-nowrap"><img data-v-31e7222c="" src="https://drive.google.com/uc?export=view&amp;id=1Csxh6szRXpu0ye3fDo0_cWQTyp1xYVeu" alt="" class="img-fluid pre-icon">შეფასებები</a> <!---->
+
+    </li>
+
+    <li data-v-31e7222c="" class="nav-item">
+
+        <a data-v-31e7222c="" href="/my-applications/" class="nav-link text-nowrap"><img data-v-31e7222c="" src="https://drive.google.com/uc?export=view&amp;id=1Csxh6szRXpu0ye3fDo0_cWQTyp1xYVeu" alt="" class="img-fluid pre-icon">განცხადებები</a> <!---->
+    </li>
+
+    <li data-v-31e7222c="" class="nav-item">
+        <a data-v-31e7222c="" href="/exam-view/" class="nav-link text-nowrap"><img data-v-31e7222c="" src="https://drive.google.com/uc?export=view&amp;id=1Csxh6szRXpu0ye3fDo0_cWQTyp1xYVeu" alt="" class="img-fluid pre-icon">ნამუშევრები</a>
+    <!----></li>
+</ul>
+</div></nav>`
+;
 
         document.querySelector("aside").innerHTML = `<div class="d-flex justify-content-center"><div class="circle-border "><div class="circle"><a href="/"><img src="https://drive.google.com/uc?export=view&amp;id=1Csxh6szRXpu0ye3fDo0_cWQTyp1xYVeu" alt="profile-image" class="img-fluid"></a></div></div></div>
  <div class="grid-lay">  
@@ -56,7 +82,9 @@ function show() {
             "ბაჯიაშვილი ანასტასია",
             "მარიამ გაგელიძე",
         ];
-      
+        var yellows = [
+            "მედეა ჯანჯღავა"
+        ]      
         const timer = setTimeout(main, 1000);
 
         // ფუნქცია იწყება 100 მილიწამში
@@ -73,14 +101,21 @@ function show() {
                 var msgs = document.querySelectorAll(".msg__item");
                 msgs.forEach(function (msg) {
                     var sender = msg.querySelector(".sender").textContent;
-                    if (!senders.includes(sender)) {
-                        console.log(msg)
+                    if (yellows.includes(sender) ) {
                         msg.remove();
+                    }
+                    if (!senders.includes(sender) && !yellows.includes(sender)  ){
+                        msg.querySelector(".sender").classList.add("yellow-msg")
                     }
                 });
 
                 document.querySelector(".conversation__start-inner button img").setAttribute("src", "https://drive.google.com/uc?export=view&id=1O13dax4VnwzhVQMQdMgnnFV0eWeVV8ZY");
                 document.querySelector(".btn-round-attachment img").setAttribute("src", "https://drive.google.com/uc?export=view&id=1Jre49HSZf8v-U6FzFYwUfeRIW235N0AG");
+            }
+            else if (url.includes("my-program")  ){
+               document.querySelectorAll("td a").forEach(function(el){
+                el.style.color = "#6d0053 !important"
+               })
             }
         }
 }
@@ -90,6 +125,7 @@ window.addEventListener("load", (event) => {
     }catch{}
     
     document.querySelector("html").style.opacity = "1";
+   
 });
 
 // nav.navbar.d-flex.align-items-center.navbar__sm.fixed-top.d-block.d-sm-none
